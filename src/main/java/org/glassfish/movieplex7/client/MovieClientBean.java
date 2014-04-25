342aa19c9bfb33d1e8d74a5c1f35f55ed281e8b7
magang <magang@magang-HP.fritz.box>
2014-04-25 13:23:21 +0200

    Revert "logic for Viewing Movies added"
    This reverts commit 79cc4a624abc960cc5f75e254187c103ddbfd0d1.

--- src/main/java/org/glassfish/movieplex7/client/MovieClientBean.java
+++ src/main/java/org/glassfish/movieplex7/client/MovieClientBean.java
@@ -1,56 +1,39 @@
+
 package org.glassfish.movieplex7.client;
 
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
+import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import org.glassfish.movieplex7.entities.Movie;
 
 /**
  *
  * Java Magazine January/February 2014
- * <p/>
+ * 
  * @author maggioni
  */
+
 @Named
 @SessionScoped
-public class MovieClientBean implements Serializable
-{
-
-    @Inject
-    MovieBackingBean bean;
-    
+public class MovieClientBean implements Serializable {
+   
     Client client;
     WebTarget target;
-
+  
     @PostConstruct
-    public void init()
-    {
+    public void init() {
         client = ClientBuilder.newClient();
         target = client.target("http://localhost:8080/moviplex7/webresources/movie/");
     }
-
+    
     @PreDestroy
-    public void destroy()
-    {
+    public void destroy() {
         client.close();
     }
-
-    public Movie[] getMovies()
-    {
-        return target.request().get(Movie[].class);
-    }
-
-    public Movie getMovie()
-    {
-        Movie m = target.path("{movie}").resolveTemplate("movie", bean.getMovieId()).request().get(
-                Movie.class);
-        return m;
-    }
+    
+    
 }
